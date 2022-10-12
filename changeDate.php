537ccf704be924ba3bed01e0272e80b89ba4f57b
Nahuel Galeano <40505451+SeveralTool@users.noreply.github.com>
2022-10-12 13:33:30 -0300

    Update and rename change date to changeDate.php

--- changeDate.php
+++ changeDate.php
@@ -1,8 +1,12 @@
+    
+    <?php
+    
     $CurrentDate = date('Y-m-d'); //FECHA ACTUAL
     $Days = 10; //NUMERO DE DIAS QUE QUIERES AGREGAR
     
     $newDate = strtotime("+$Days days", strtotime($CurrentDate)); // AGREGA LOS DIAS CORRESPONDIENTES
     $newDate = date('Y-m-d', $newDate); // ASIGNA UN FORMATO DATE A newDate
     
+    ?>
     
     //SeveralTool
